028b0bfe2cfef7d49d04d99963324ccff56ee61c
Karl <enriquezkharl14@gmail.com>
2023-08-07 15:14:04 +0800

    saving data in firestore

--- src/features/auth/Register.tsx
+++ src/features/auth/Register.tsx
@@ -1,11 +1,34 @@
+'use client';
+
 import Card from '@/components/Card';
 import RegisterForm from './forms/RegisterForm';
 import { BsPersonCircle } from 'react-icons/bs';
 import Link from 'next/link';
 
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { collection, addDoc } from 'firebase/firestore';
+import app, { db } from '@/services/firebase';
+import Button from '@/components/Button';
+
+const auth = getAuth(app);
 
 export default function Register() {
+  const handleAddUser = async () => {
+    try {
+      const newData = await createUserWithEmailAndPassword(auth, 'Email@mail.com', 'Helloasdasd');
+
+      const docRef = await addDoc(collection(db, 'users'), {
+        id: newData.user.uid,
+        name: 'Kharl ',
+        email: newData.user.email,
+      });
+
+      console.log(newData, docRef);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
       <Card className="space-y-4 lg:w-1/4 md:w-1/2 w-full">
@@ -22,6 +45,8 @@ export default function Register() {
             Already have an account? Log in instead.
           </Link>
         </div>
+
+        <Button onClick={handleAddUser}>Test Add</Button>
       </Card>
     </div>
   );
